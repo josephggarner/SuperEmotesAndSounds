9a99f476fda1a94574bf230f02d577433e120655
chrishull91 <chrishull91@sbcglobal.net>
2020-09-28 11:10:25 -0400

    Made buttons play advertised emote when clicked (pretty friggin cool).
    TODO:add cooldown option on received emote messages, with ability to turn off cooldown.

--- XML_bootstrap.lua
+++ XML_bootstrap.lua
@@ -12,11 +12,22 @@ end
 function MyModScrollBar_Update()
   local line; -- 1 through 5 of our window to scroll
   local lineplusoffset; -- an index into our data calculated from the scroll offset
+
   FauxScrollFrame_Update(MyModScrollBar,#SESEmotesLIB,10,16);
   for line=1,10 do
     lineplusoffset = line + FauxScrollFrame_GetOffset(MyModScrollBar);
     if lineplusoffset <= #SESEmotesLIB then
       getglobal("MyModEntry"..line):SetText(MyModData[lineplusoffset][1].." - "..MyModData[lineplusoffset][2]);
+	  getglobal("MyModEntry"..line):SetScript("OnClick", function(self) 
+				emotemsg = "EMOTE-"..MyModData[lineplusoffset-(10-line)][1]
+				broadcastSES(emotemsg)
+					--print(emotemsg)
+					--print("line "..line.."    lineplusoffset "..lineplusoffset.."    Offset "..FauxScrollFrame_GetOffset(MyModScrollBar))
+					--print("calc'd line "..lineplusoffset-(10-line))
+				print("Playing Emote.")
+				SESPlayEmoteSound(SESEmotesLIB,lineplusoffset-(10-line))
+		end)
+
       getglobal("MyModEntry"..line):Show();
     else
       getglobal("MyModEntry"..line):Hide();
