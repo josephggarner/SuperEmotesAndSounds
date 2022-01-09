64f02baaeb92820aa9914a93815c919f0a05be7b
josephggarner <josephggarner@gmail.com>
2022-01-08 22:29:22 -0500

    Updated frame shown for list command

--- SuperEmotesAndSounds.lua
+++ SuperEmotesAndSounds.lua
@@ -5,11 +5,14 @@ local superframe = SuperEmotesFrame
 
 superframe:Hide()
 superframe:SetScale(1)
---superframe:SetBackdrop( {			--CURRENTLY BROKEN
---	bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
---	edgeFile = "Interface/Tooltips/UI-Tooltip-Border", tile = true, tileSize = 16, edgeSize = 16, 
---	insets = { left = 4, right = 4, top = 4, bottom = 4 }
---});
+superframe:SetBackdrop( {
+	bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background-Dark", 
+	edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+	tile = true,
+	tileSize = 16,
+	edgeSize = 16, 
+	insets = { left = 4, right = 4, top = 4, bottom = 4 }
+});
 
 --###Slash command Handler###--
 SLASH_SES1, SLASH_SES2 = '/ses', '/superemotes';
